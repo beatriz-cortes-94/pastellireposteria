8142345b69149b8c86f8aa6c0f6afb505a4a6d59
Betty Cortés <bettycortes@Bettys-MacBook-Pro.local>
2020-07-28 19:56:56 -0500

    Updates

---
+++ precache-manifest.e9ce119851a1546ffbece0d16865329e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f6f18d13b0daaf3443b554107e12af",
+    "url": "/pastellireposteria/index.html"
+  },
+  {
+    "revision": "cc7b8b6426ddf2cbe5bb",
+    "url": "/pastellireposteria/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "566e49c39e08cb5985cc",
+    "url": "/pastellireposteria/static/css/main.5e11e69f.chunk.css"
+  },
+  {
+    "revision": "cc7b8b6426ddf2cbe5bb",
+    "url": "/pastellireposteria/static/js/2.eebcb988.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/pastellireposteria/static/js/2.eebcb988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566e49c39e08cb5985cc",
+    "url": "/pastellireposteria/static/js/main.a9153b76.chunk.js"
+  },
+  {
+    "revision": "2eb3cd3740a0e3d47723",
+    "url": "/pastellireposteria/static/js/runtime-main.0ee8038e.js"
+  },
+  {
+    "revision": "75a97acf4ecde6859d283a74aac2e551",
+    "url": "/pastellireposteria/static/media/about-us.75a97acf.jpg"
+  },
+  {
+    "revision": "f1e4fb232ef6c1a40ade37b278e79609",
+    "url": "/pastellireposteria/static/media/avena.f1e4fb23.jpg"
+  },
+  {
+    "revision": "a882e6c1045de7940c2aaf31382fd76f",
+    "url": "/pastellireposteria/static/media/chocochip.a882e6c1.jpg"
+  },
+  {
+    "revision": "93c33b1e8909b8c979ab9055b529f9b8",
+    "url": "/pastellireposteria/static/media/doble_chocolate.93c33b1e.jpg"
+  },
+  {
+    "revision": "906a58437b10872c0650d36f9fd9e7cf",
+    "url": "/pastellireposteria/static/media/landing.906a5843.jpeg"
+  },
+  {
+    "revision": "fdca4a39647032450b2e57198eadf667",
+    "url": "/pastellireposteria/static/media/products-hero.fdca4a39.jpeg"
+  },
+  {
+    "revision": "bdc741edefb357c847c0d4c7db0f2373",
+    "url": "/pastellireposteria/static/media/red_velvet.bdc741ed.jpg"
+  },
+  {
+    "revision": "2e60f88cefb08dc46b0b573b9ef83de1",
+    "url": "/pastellireposteria/static/media/sabor_del_mes.2e60f88c.jpg"
+  },
+  {
+    "revision": "4a0f54d722add6e3308a98988aae6ec5",
+    "url": "/pastellireposteria/static/media/sin_azucar.4a0f54d7.jpg"
+  }
+]);
